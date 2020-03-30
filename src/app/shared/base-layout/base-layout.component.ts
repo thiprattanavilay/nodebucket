55153bec765a1ca7989dc5976dacd33299d570ab
Thip Rattanavilay <trattanavilay@my365.bellevue.edu>
2020-03-29 21:41:19 -0700

    Updated nodebucket

--- src/app/shared/base-layout/base-layout.component.ts
+++ src/app/shared/base-layout/base-layout.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-base-layout',
   templateUrl: './base-layout.component.html',
@@ -12,13 +13,15 @@ import { Router } from '@angular/router';
 export class BaseLayoutComponent implements OnInit {
   year: number = Date.now();
 
+
   constructor(private cookieService: CookieService, private router: Router) {
 
   }
 
+
+
   ngOnInit() {
   }
-
   /**
    * Delete cookie and redirect to signin page.
    */
